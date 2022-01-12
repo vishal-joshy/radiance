ed81f4ac99f4840a0102cbe68951955a528b5b83
vishaljoshy18 <1812-vishal@protonmail.com>
2022-01-12 15:19:54 +0530

    Add styles to DoubleSlider

--- src/components/Slider/DoubleSlider.tsx
+++ src/components/Slider/DoubleSlider.tsx
@@ -22,8 +22,10 @@ function DoubleSlider({ id, labelOne, labelTwo, handleChange, getValue }: Props)
   }, [sliderValueTwo, sliderValueOne]);
 
   return (
-    <div>
+    <div >
+      <div className={style.sliderContainer}>
       <label htmlFor="">{labelOne}</label>
+      <div>
       <input
         type="range"
         min={0}
@@ -32,11 +34,18 @@ function DoubleSlider({ id, labelOne, labelTwo, handleChange, getValue }: Props)
         onChange={({ target }) => setSliderValueOne(parseInt(target.value))}
       />
       <input
-        type="text"
+        className={style.textInput}
+        type="number"
+        max={100}
+        min={0}
         value={sliderValueOne}
         onChange={({ target }) => setSliderValueOne(parseInt(target.value))}
       />
+      </div>
+      </div>
+      <div className={style.sliderContainer}>
       <label htmlFor="">{labelTwo}</label>
+      <div>
       <input
         type="range"
         min={0}
@@ -45,12 +54,22 @@ function DoubleSlider({ id, labelOne, labelTwo, handleChange, getValue }: Props)
         onChange={({ target }) => setSliderValueTwo(parseInt(target.value))}
       />
       <input
-        type="text"
+        className={style.textInput}
+        type="number"
+        max={100}
+        min={0}
         value={sliderValueTwo}
         onChange={({ target }) => setSliderValueTwo(parseInt(target.value))}
       />
+      </div>
+      </div>
     </div>
   );
+}
+
+const style={
+  sliderContainer:"flex justify-between p-2 2-80",
+  textInput:'w-10'
 }
 
 export default DoubleSlider;
