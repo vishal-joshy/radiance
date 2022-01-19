74bd3147e6feb2c69d2117920dc442bdf41f8229
vishaljoshy18 <1812-vishal@protonmail.com>
2022-01-19 20:32:50 +0530

    Add string utility function tests.

--- src/utilities/string.test.js
+++ src/utilities/string.test.js
@@ -1,6 +1,19 @@
-import {separateSlash} from './string';
+import { separateSlash, createKeyPair, getStringValue } from "./string";
 
-it("should separate slash from string",()=>{
+describe("string utility functions", () => {
+  it("should separate slash from string", () => {
     const routeString = "/routepath";
-    expect(separateSlash(routeString)).toBe("routepath")
-})
+    expect(separateSlash(routeString)).toBe("routepath");
+  });
+  it("should create key pair", () => {
+    expect(createKeyPair("animal", "dog")).toStrictEqual({ animal: "dog" });
+  });
+  it("should create string from json and callback function", () => {
+    const testData = {
+      animal: "dog",
+      color: "red",
+    };
+    const callback = (key) => `${key}=${testData[key]}`;
+    expect(getStringValue(testData, callback)).toStrictEqual("animal=dog\ncolor=red");
+  });
+});
