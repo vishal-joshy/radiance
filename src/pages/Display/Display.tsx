6d137313ab7970cd2bdb49b9236923ddf1c094b2
vishaljoshy18 <1812-vishal@protonmail.com>
2022-03-03 12:52:25 +0530

    Add pages.

---
+++ src/pages/Display/Display.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import DropDown from 'components/DropDown/DropDown';
+import Layout from 'components/Layout';
+
+type Props = {};
+
+function Display({}: Props) {
+  return (
+    <Layout>
+      <h1>Display</h1>
+    </Layout>
+  );
+}
+
+export default Display;
---
+++ src/pages/Subtitles/Subtitles.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import DropDown from 'components/DropDown/DropDown';
+import Layout from 'components/Layout';
+
+type Props = {};
+
+function Subtitles({}: Props) {
+  return (
+    <Layout>
+      <h1>Subtitles</h1>
+    </Layout>
+  );
+}
+
+export default Subtitles;
