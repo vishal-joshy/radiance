49296fa0ca53ab3187deb3678e18e7d913e05d3d
vishaljoshy18 <1812-vishal@protonmail.com>
2022-03-23 23:31:03 +0530

    Change BrowserRouter to HashRouter for gh-pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './App.css';
 import General from './pages/General/General';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Video from './pages/Video/Video';
-import Audio from 'pages/Audio/Audio';
-import Stream from 'pages/Stream/Stream';
-import Subtitles from 'pages/Subtitles/Subtitles';
-import Display from 'pages/Display/Display';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Video from "./pages/Video/Video";
+import Audio from "pages/Audio/Audio";
+import Stream from "pages/Stream/Stream";
+import Subtitles from "pages/Subtitles/Subtitles";
+import Display from "pages/Display/Display";
 
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
+    <div className="App">
+      <HashRouter>
         <Routes>
-          <Route path='/' element={<General />} />
-          <Route path='/video' element={<Video />} />
-          <Route path='/audio' element={<Audio />} />
-          <Route path='/stream' element={<Stream />} />
-          <Route path='/subtitles' element={<Subtitles />} />
-          <Route path='/display' element={<Display />} />
+          <Route path="/" element={<General />} />
+          <Route path="/video" element={<Video />} />
+          <Route path="/audio" element={<Audio />} />
+          <Route path="/stream" element={<Stream />} />
+          <Route path="/subtitles" element={<Subtitles />} />
+          <Route path="/display" element={<Display />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
